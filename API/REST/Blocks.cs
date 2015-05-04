5718951f9ce9ca4abf0804862be5f46a8fa856fa
syuilo <syuilotan@yahoo.co.jp>
2015-05-05 06:59:14 +0900

    Fix method names

--- API/REST/Blocks.cs
+++ API/REST/Blocks.cs
@@ -13,7 +13,7 @@ namespace Twitch
 		/// <param name="screen_name">ブロックするユーザーのScreenName。</param>
 		/// <param name="id">ブロックするユーザーのID。</param>
 		/// <returns>ブロックされたユーザー</returns>
-		public async Task<User> Create(string screen_name = null, Int64? id = null)
+		public async Task<User> BlocksCreate(string screen_name = null, Int64? id = null)
 		{
 			var query = new StringDictionary();
 			query["screen_name"] = screen_name;
@@ -31,7 +31,7 @@ namespace Twitch
 		/// <param name="screen_name">ブロックを解除するユーザーのScreenName。</param>
 		/// <param name="id">ブロックを解除するユーザーのID。</param>
 		/// <returns>ブロックを解除されたユーザー</returns>
-		public async Task<User> Destroy(string screen_name = null, Int64? id = null)
+		public async Task<User> BlocksDestroy(string screen_name = null, Int64? id = null)
 		{
 			var query = new StringDictionary();
 			query["screen_name"] = screen_name;
